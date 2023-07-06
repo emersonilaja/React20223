b17accdbfe91574cd00969874da638fe73f45c64
Tecnologías de la Información <tic.calama@inacap.cl>
2023-07-06 16:08:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, {useEffect, useState} from "react"
+
+const App = () =>{
+     return (
+       <>
+          <h1>Título</h1>
+          <p>Hola World!</p>
+       </>
+       
+     )
+}
+export default App
